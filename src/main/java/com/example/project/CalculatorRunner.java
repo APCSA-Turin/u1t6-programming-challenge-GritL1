61217e8cf9e02b23498ad242c2b8eb5f2ea1983f
GritL1 <bingyanl@nycstudents.net>
2025-10-07 17:59:06 +0000

    a

--- src/main/java/com/example/project/CalculatorRunner.java
+++ src/main/java/com/example/project/CalculatorRunner.java
@@ -5,8 +5,13 @@ public class CalculatorRunner {
 
         //test your program here
         //1. create an instance of the calculator class
-
+        Calculator calc = new Calculator("3");
         //2. call any methods of that class to test
-
+        System.out.println(calc.performOperation("+", 8, 8));
+        System.out.println(calc.divisibleBy(16, 8));
+        System.out.println(calc.coordinatePair(1,2));
+        System.out.println(calc.absoluteValue(4,6));
+        System.out.println(calc.average(5,8));
+        System.out.println(calc.info());
     }
 }
